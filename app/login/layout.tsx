5b923d0a1b882209fc409cf6962e94556cbb38a3
ajetunmobile <ayoajetunmobi78@gmail.com>
2026-08-08 05:30:44 +0300

    html cannot be element of body corrected

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -24,8 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <body className={`${roboto.className}`}>
+    <div className={`${roboto.className}`}>
         {children}
-    </body>
+    </div>
   );
 }
